b3e0d75f372b067e90205643e9bd582b666f4e84
Trina <tcjcastillo@gmail.com>
2017-07-10 22:58:27 -0700

    WIP gallery work

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  public images = [
+    'cake1.jpg',
+    'cake2.jpg',
+    'cake3.jpg',
+    'cake4.jpg',
+    'cake5.jpg',
+    'cake6.jpg',
+    'cake7.jpg',
+    'birthday1.jpg',
+    'birthday2.jpg',
+    'birthday3.jpg',
+    // 'christmas1.jpg',
+  ];
   constructor() { }
 
   ngOnInit() {
